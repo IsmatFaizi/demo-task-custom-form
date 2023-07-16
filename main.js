dd454dbcc2e6131c4ed9e3c37b8d8c4e7bf07d8e
ismatfaizi <ismatfaizi25@gmail.com>
2023-07-16 17:16:33 +0200

    add a field counter that act like fields id

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 $(document).ready(function () {
+  let fieldCounter = 0;
+
   // Function to add a new text field
   $("#add-text-field").on("click", function () {
+    fieldCounter++;
     let fieldName = prompt("Enter a name for the text field:");
-    let newField = `<div id="field-${fieldName}"><label for="text-${fieldName}">${fieldName}:</label><input type="text" id="text-${fieldName}" name="${fieldName}"/></div>`;
+    let newField = `<div id="field-${fieldCounter}">
+    <label for="text-${fieldCounter}">${fieldName}:</label>
+    <input type="text" id="text-${fieldCounter}" name="${fieldName}"/>
+    <button class="remove" data-field="field-${fieldCounter}">Remove</button>
+    </div>`;
     $("#form-fields").append(newField);
   });
+
 });
